4cfc54dd79972aa176d5177715abbbf53fabc70c
zimabluee <hbendale23@gmail.com>
2022-03-10 09:38:05 -0800

    Fixed corrections

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
+/*
+ *main - body 
+ *return
+*/
 int main(void)
 {
 puts("\"Programming is like building a multiligual puzzle")
-return(0);
+return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
+/**
+ * main - body
+ * return
+ */
 int main(void)
 {
 printf("with proper grammer, but the outcome is a piece of art,\n");
-return(0);
+return (0);
 }
