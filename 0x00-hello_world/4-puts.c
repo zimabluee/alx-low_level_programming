1a6f0aeabdbbe499c8717bd7531c96f96ab8c4bd
zimabluee <hbendale23@gmail.com>
2022-03-10 09:46:39 -0800

    Added comments

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 /*
  *main - body 
- *return
-*/
+ *return: 0
+ */
 int main(void)
 {
 puts("\"Programming is like building a multiligual puzzle")
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 /**
  * main - body
- * return
+ * return: 0
  */
 int main(void)
 {
-printf("with proper grammer, but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
