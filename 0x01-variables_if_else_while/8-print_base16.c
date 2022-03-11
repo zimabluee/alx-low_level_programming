660c08e04c1c7259615c6183df33a2a985ca0bf4
Hebron <72515088+zimabluee@users.noreply.github.com>
2022-03-11 22:20:12 +0300

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
- * Return:0
+ *main - entry point
+ *
+ *Description:prints numbers of base16 in lowercase
+ *
+ *Return: Always zero
  */
 int main(void)
 {
-	int i, y;
-	
-	for (i = 0; i < 10; i++)
-	{
-		putchar (i + '0');
-	}
-	for (y = 'a'; y <= 'f'; y++)
-	{
-		putchar (y);
-	}
+	int dig, letterhex, n;
 
-putchar('\n');
-return (0);
+	for (dig = 48, n = 0; n < 10; dig++, n++)
+	{
+		putchar(dig);
+	}
+	for (letterhex = 97, n = 1; n < 7; letterhex++, n++)
+	{
+		putchar(letterhex);
+	}
+	putchar(10);
+	return (0);
 }
