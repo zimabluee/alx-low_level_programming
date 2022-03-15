324932601060ab697beecbf0cfc99ebbc58f8d85
Hebron <72515088+zimabluee@users.noreply.github.com>
2022-03-15 20:52:09 +0300

    Create 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ *print_alphabet_x10 - prints alphabet 10 times
+ *
+ *Description: prints the alphabet in lowercase ten times
+ *
+ *Return: void
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		char ch;
+
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
+	}
+}
