b8506a2758ad6ba75572c4b0ac932ed4b8345560
Hebron <72515088+zimabluee@users.noreply.github.com>
2022-03-21 21:02:20 +0300

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,25 +1,28 @@
 #include "main.h"
 /**
- * rev_string - reverses a string
- * @s: My School
+ * rev_string - prints reversed string, followed by a new line
+ * @s: pointer to the string to print
  */
+
 void rev_string(char *s)
 {
-	int i, j, temp, length;
 
-	for (len = 0; s[len] != '\0'; len++)
-		;
-	
-	length = i;
-	i = 0;
-	j = length - 1;
+int length, i, half;
+char temp;
 
-	while (i < j)
-	{
-		temp = s[i];
-		s[i] = s[j];
-		s[j] = temp;
-		i++;
-		j--;
-	}
+for (len = 0; s[length] != '\0'; length++)
+;
+
+i = 0;
+
+half = length / 2;
+
+while (half--)
+{
+	temp = s[length - i - 1];
+	s[len - i - 1] = s[i];
+	s[i] = temp;
+	i++;
+}
+
 }
