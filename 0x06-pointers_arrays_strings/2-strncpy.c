e7b6bafc2686392435e97aeec82f4e30640a3847
Hebron <72515088+zimabluee@users.noreply.github.com>
2022-03-24 21:21:07 +0300

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,14 +9,15 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-   int i;
-   
-   for (i = 0; src[i] != '\0' && i < n; i++)
-   {
+	int i;
+
+	for (i = 0; src[i] != '\0' && i < n; i++)
+	{
 		dest[i] = src[i];
-   }
-   for (; i < n; i++)
-   {
+	}
+	for (; i < n; i++)
+	{
 		dest[i] = '\0';
-   }
+	}
+	return (dest);
 }
