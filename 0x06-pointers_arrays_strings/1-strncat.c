3304df8d3d00e76e39faa9350d4c9d8454f5190a
Hebron <72515088+zimabluee@users.noreply.github.com>
2022-03-24 11:04:11 -0700

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,10 +13,14 @@ char *_strncat(char *dest, char *src, int n)
 	int i, length, j;
 
 	for (i = 0; dest[i] != '\0'; i++)
-    length++;
+	{
+		length++;
+	}
   
 	for (j = 0; src[j] != '\0' && j < n; j++)
+	{
 		dest[i + j] = src[j];
+	}
   
 	dest[i + j] = '\0';
 	return (dest);
