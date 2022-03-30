7253d08344b3258cd7430f419472dbaec95da6c1
Hebron <72515088+zimabluee@users.noreply.github.com>
2022-03-30 15:25:22 +0300

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,13 +9,13 @@
  */
 void _puts_recursion(char *s)
 {
-  if (*s == '\0')
-  {
-     _putchar ('\n');
-  }
-  else
+	if (*s == '\0')
 	{
-    _putchar(*s);
-    _puts_recursion(s + 1);
+		_putchar ('\n');
+	}
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
 }
