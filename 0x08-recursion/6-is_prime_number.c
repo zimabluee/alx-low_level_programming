8ba762abba08ef848296b66d21a7beb663f67ead
Hebron <72515088+zimabluee@users.noreply.github.com>
2022-03-30 20:37:46 +0300

    Create 6-is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ *prime_check - returns 1 if n is prime
+ *@i: input
+ *@j:input
+ *Return:1 - prime ,0 - otherwise
+ *
+ */
+int prime_check( int i, int j)
+{
+	if (j < 2 || j % i == 0)
+	{
+		return (0);
+	}
+	else if (j > i / 2)
+	{
+		return (1);
+	}
+	else 
+	{
+		return (prime_check( i + 1, j));
+	}
+}
+/**
+ *is_prime_number - returns 1 if a number is prime
+ *
+ *@n:input
+ *Return: 1-prime,0-otherwise
+ *
+ */
+int is_prime_number(int n)
+{
+	if (n == 2)
+	{
+		return (1);
+	}
+	return (prime_check(2, n));
+}
