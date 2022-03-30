0aaecc213b33bbbf3242caccee9295ca6c5bbd7e
Hebron <72515088+zimabluee@users.noreply.github.com>
2022-03-30 17:39:12 +0300

    Create 5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * square_root - find the natural square root
+ * @i: number
+ * @j: incrementor
+ *
+ * Return: the number found
+ */
+int _sqrt(int i, int j)
+{
+	if (i * i == j)
+	{
+		return (i);
+	}
+	else if (i * i > j)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (_sqrt(i + 1, j));
+	}
+}
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number
+ * Return: the natural square root found
+ */
+int _sqrt_recursion(int n)
+{
+	int a = 1;
+
+	return (_sqrt(a, n));
+}
