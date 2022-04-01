64b46da4fbced8222c4628d0947cb95cb16f0266
Hebron <72515088+zimabluee@users.noreply.github.com>
2022-04-01 22:19:23 +0300

    Update 2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,13 +1,17 @@
+#include "main.h"
 #include <stdio.h>
 /**
- *main - print one argument per line
+ *main - prints all arguements passed on to it followed by newline
+ *
  *@argc:arguement count
  *@argv:arguement vector
+ *Return:0(success)
  */
 int main(int argc, char *argv[])
 {
 	int i;
-	for(i = 1; i < argc; i++)
+
+	for (i = 0; i < argc; i++)
 	{
 		printf("%s\n", argv[i]);
 	}
