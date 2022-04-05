43dffbe294a8a298d88f9315b201b2c5bc1eb9dd
Hebron <72515088+zimabluee@users.noreply.github.com>
2022-04-05 15:23:13 +0300

    Update 2-strlen.c

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen - give the length of a string
+ * @s: the string
+ * Return: the length of a string
+ */
+int _strlen(char *s)
+{
+int i = 0;
+while (s[i])
+	i++;
+
+return (i);
+}
