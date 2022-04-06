66de8879cd8aeeade098672d369f39eeceda01a4
Hebron <72515088+zimabluee@users.noreply.github.com>
2022-04-06 20:23:50 +0300

    Update 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,53 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
-/**
- *main - prints the minimum number of coins to make change for an amount of money
- *
- *@argc:argument count
- *@argv:argument vector
- *
- *Return:0(success)
- *
- */
-int main(int argc, char *argv[])
-{
-	int amount, coins;
 
-	if (argc != 2)
+/**
+ * main - function to print out the change that needs to given
+ * @argc: number of arguments to be passed
+ * @argv: number of variables to calculate
+ * Return: always 0 for sucess
+ */
+
+int main(int argc, char **argv)
+{
+	int total, change;
+
+	if (argc < 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	amount = atoi(argv[1]);
-	if (amount < 0)
+
+	change = atoi(argv[1]);
+
+	for (total = 0; change > 0; total++)
 	{
-		printf("%d\n", 0);
-		return (0);
+		if (change - 25 >= 0)
+			change = change - 25;
+		else if (change - 10 >= 0)
+			change = change - 10;
+		else if (change - 5 >= 0)
+			change = change - 5;
+		else if (change - 2 >= 0)
+			change = change - 2;
+		else if (change - 1 >= 0)
+			change = change - 1;
 	}
-	if (amount % 25 >= 0)
-	{
-		coins = amount / 25;
-		amount = amount % 25;
-	}
-	if (amount % 10 >= 0)
-	{
-		coins += amount / 10;
-		amount = amount % 10;
-	}
-	if (amount % 5 >= 0)
-	{
-		coins += amount / 5;
-		amount = amount % 5;
-	}
-	if (amount % 2 >= 0)
-	{
-		coins += amount / 2;
-		amount = amount % 2;
-	}
-	if (amount % 1 >= 0)
-	{
-		coins += amount / 1;
-	}
-	printf("%d\n", coins);
+	printf("%d\n", total);
 	return (0);
 }
