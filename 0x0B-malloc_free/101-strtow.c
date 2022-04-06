ea2ef9dbebda2fec7fd100e5ffa46c1851eb3d57
Hebron <72515088+zimabluee@users.noreply.github.com>
2022-04-06 20:11:51 +0300

    Update 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,36 +1,97 @@
-#include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "holberton.h"
 /**
- *argstostr -  returns a pointer to an array of strings
+ *word_len - finds the length of a word
+ *@str:string to test
  *
- *@ac:arguement count
- *@av:arguement vector
+ *Return:int
+ */
+int word_len(char *str)
+{
+	int i = 0, len = 0;
+
+	while (*(str + i) && *(str + i) != ' ')
+	{
+		len++;
+		i++;
+	}
+	return (len);
+}
+/**
+ *word_count - counts the number of words
  *
- *Return:pointer to a new string or null if empty
+ *@str:input
+ *
+ *Return:(no. of words)
+ *
+ */
+int word_count(char *str)
+{
+	int i = 0, len = 0, count = 0;
+
+	for (i = 0; *(str + i); i++)
+	{
+		len++;
+	}
+	for (i = 0; i < len; i++)
+	{
+		if (*(str + i) != ' ')
+		{
+			count++;
+			i += word_len(str + i);
+		}
+	}
+	return (count);
+}
+/**
+ *strtow - splits a string into words
+ *
+ *@str:input
+ *
+ *Return:0 - success
+ *
  */
 char **strtow(char *str)
 {
-	int i = 0, length = 0;
-	char *s;
-	
-	if (str == NULL || str == "")
+	int i, words, w, letters, l;
+	char **p;
+
+	if (str == NULL || str[0] == '\0')
 	{
 		return (NULL);
 	}
-	for (i = 0; str[i] == '\0'; i++)
-	{
-		length++;
-	}
-	s = malloc((sizeof(char) * length) + 1);
-	if (s == NULL)
+	words = word_count(str);
+	if (words == 0)
 	{
 		return (NULL);
 	}
-	for (i = 0; str[i]; i++)
+	p = malloc(sizeof(char *) * (words + 1));
+	if (p == NULL)
 	{
-		s[i] = str[i];
+		return (NULL);
 	}
-	s[i] = '\0';
-	return (s);
+	for (i = 0; i < words; i++)
+	{
+		while (*(str + w) == ' ')
+		{
+			w++;
+		}
+		letters = word_len(str + w);
+		p[i] = malloc(sizeof(char) * (letters + 1));
+		if (p[i] == NULL)
+		{
+			for (; i >= 0; i--)
+				free(p[i]);
+			free(p);
+			return (NULL);
+		}
+		for (l = 0; l < letters; l++)
+		{
+			p[i][l] = str[w++];
+		}
+		p[i][l] = '\0';
+	}
+	p[i] = NULL;
+	return (p);
 }
