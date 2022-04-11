43f7dbf206d6444004dc666fbf1c560585456f2b
Hebron <72515088+zimabluee@users.noreply.github.com>
2022-04-11 20:05:47 +0300

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,17 +1,20 @@
 #ifndef DOG_H
 #define DOG_H
+
 /**
  *struct dog - structure for dog variables
- *@name: name of the dog
- *@age: age of the dog
- *@owner: owner of the dog
+ *@name:name
+ *@age:age of dog
+ *@owner:name of owner
  */
- 
 typedef struct dog
 {
-	char * name;
+	char *name;
 	float age;
-	char * owner;
-} 
-dog;
+	char *owner;
+} dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
