89d62df5c238d2e9cc0572cf6bc6a7f2cf657bf2
Hebron <72515088+zimabluee@users.noreply.github.com>
2022-04-11 20:31:40 +0300

    Update 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -10,10 +10,10 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d != '\0')
+	if (d)
 	{
-		(*d).name = name;
+		d->name = name;
 		(*d).age = age;
-		(*d).owner = owner;
+		d->owner = owner;
 	}
 }
