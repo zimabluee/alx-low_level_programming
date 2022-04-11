da630b30ab45fd5c32cc93a168649ac77d082187
Hebron <72515088+zimabluee@users.noreply.github.com>
2022-04-11 15:48:31 +0300

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,17 @@
-# include <stdlib.h>
-struct dog
+#ifndef DOG_H
+#define DOG_H
+/**
+ *struct dog - stores
+ *@name: name of the dog
+ *@age: age of the dog
+ *@owner: owner of the dog
+ */
+ 
+typedef struct dog
 {
-	char * = name;
-	float = age;
-	char * = owner;
+	char * name;
+	float age;
+	char * owner;
 }
+#endif
+
