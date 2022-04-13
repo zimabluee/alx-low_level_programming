2129e95c0ea563a4d277d26647a64d1d10716bcc
Hebron <72515088+zimabluee@users.noreply.github.com>
2022-04-13 16:15:12 +0300

    Create 2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,17 @@
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+	if (size <= 0)
+	{
+		return (-1);
+	}
+	for (i = 0; i <= size; i++)
+	{
+		if ((*cmp)(array[i]) != 0)
+		{
+			return (i);
+			break;
+		}
+	}
+	return (-1);
+}
