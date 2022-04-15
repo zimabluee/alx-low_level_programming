04af262070795f927bdf0e13e28ac0d2cf8bf212
Hebron <72515088+zimabluee@users.noreply.github.com>
2022-04-15 08:58:03 +0300

    Update 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,44 +2,52 @@
 #include <stdlib.h>
 #include <stdarg.h>
 /**
- * print_all - prints anything.
- * @format: types of arguments passed to the function.
- * Return: nothing.
+ *print_all - prints any type of character
+ *
+ *@format:format
+ *
+ *Return:void
  */
-void print_all (const char *const format, ...)
+void print_all(const char * const format, ...)
 {
-	va_list ap;
-	char *s;
 	int i = 0;
-	va_start(ap, format);
-	while (format && format[i])
+	char *str, *sprtr = "";
+
+	va_list va;
+
+	va_start(va, format);
+
+	if (format)
 	{
-		switch (format[i++])
+		while (format[i] != '\0')
 		{
+			switch (format[i])
+			{
 			case 'c':
-				printf("%c", va_arg(ap, int));
+				printf("%s%c", sprtr, va_arg(va, int));
 				break;
 			case 'i':
-				printf("%d", va_arg(ap, int));
+				printf("%s%d", sprtr, va_arg(va, int));
 				break;
 			case 'f':
-				printf("%f", va_arg (ap, double));
+				printf("%s%f", sprtr, va_arg(va, double));
 				break;
 			case 's':
-				s = va_arg(ap, char *);
-				if (s == NULL)
+				str = va_arg(va, char *);
+				if (str == NULL)
 				{
-					printf("(nil)");
-					break;
+					str = "(nil)";
 				}
-				printf("%s", s);
+				printf("%s%s", sprtr, str);
 				break;
 			default:
+				i++;
 				continue;
-	    }
-		if (format[i])
-			printf(", ");
+			}
+			sprtr = ", ";
+			i++;
+		}
 	}
-    printf ("\n");
-    va_end(ap);
+	printf("\n");
+	va_end(va);
 }
